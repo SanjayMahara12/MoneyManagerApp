66d9b8faa9e5e70a0750db27bb8b2ee638ce94dd
SanjayMahara12 <sanjaymahara@gmail.com>
2021-04-14 16:12:27 +0530

    Add given classes

---
+++ MoneyManager.App/MoneyManager.App.Lib/IMoney.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.App.Lib
+{
+    interface IMoney
+    {
+        /// <summary>
+        /// The amount of money this instance represents.
+        /// </summary>
+        /// </summary>
+        decimal Amount { get; }
+
+        /// <summary>
+        /// The ISO currency code of this instance.
+        /// </summary>
+        string Currency { get; }
+    }
+}
